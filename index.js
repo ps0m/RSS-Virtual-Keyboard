4ba48212ede0eddaa04d8d9d9331c3b2849a43f1
ps0m <ps0m@tut.by>
2022-05-07 16:30:49 +0300

    feat: create DOM in js, add Basic functionality

---
+++ index.js
@@ -0,0 +1,470 @@
+const ListSetKey = [{
+  key: "`", code: "Backquote", textEn: "`", textEnShift: "~", textRu: "ё", textRuShift: "Ё", size: "normal", assignment: "symbol",
+},
+{
+  key: "1", code: "Digit1", textEn: "1", textEnShift: "!", textRu: "1", textRuShift: "!", size: "normal", assignment: "symbol",
+},
+{
+  key: "2", code: "Digit2", textEn: "2", textEnShift: "@", textRu: "2", textRuShift: "\"", size: "normal", assignment: "symbol",
+},
+{
+  key: "3", code: "Digit3", textEn: "3", textEnShift: "#", textRu: "3", textRuShift: "№", size: "normal", assignment: "symbol",
+},
+{
+  key: "4", code: "Digit4", textEn: "4", textEnShift: "$", textRu: "4", textRuShift: ";", size: "normal", assignment: "symbol",
+},
+{
+  key: "5", code: "Digit5", textEn: "5", textEnShift: "%", textRu: "5", textRuShift: "%", size: "normal", assignment: "symbol",
+},
+{
+  key: "6", code: "Digit6", textEn: "6", textEnShift: "^", textRu: "6", textRuShift: ":", size: "normal", assignment: "symbol",
+},
+{
+  key: "7", code: "Digit7", textEn: "7", textEnShift: "&", textRu: "7", textRuShift: "?", size: "normal", assignment: "symbol",
+},
+{
+  key: "8", code: "Digit8", textEn: "8", textEnShift: "*", textRu: "8", textRuShift: "*", size: "normal", assignment: "symbol",
+},
+{
+  key: "9", code: "Digit9", textEn: "9", textEnShift: "(", textRu: "9", textRuShift: "(", size: "normal", assignment: "symbol",
+},
+{
+  key: "0", code: "Digit0", textEn: "0", textEnShift: ")", textRu: "0", textRuShift: ")", size: "normal", assignment: "symbol",
+},
+{
+  key: "-", code: "Minus", textEn: "-", textEnShift: "_", textRu: "-", textRuShift: "_", size: "normal", assignment: "symbol",
+},
+{
+  key: "=", code: "Equal", textEn: "=", textEnShift: "+", textRu: "=", textRuShift: "+", size: "normal", assignment: "symbol",
+},
+{
+  key: "Backspace", code: "Backspace", textEn: "Backspace", textEnShift: "Backspace", textRu: "Backspace", textRuShift: "Backspace", size: "big", assignment: "functional",
+},
+{
+  key: "Tab", code: "Tab", textEn: "Tab", textEnShift: "Tab", textRu: "Tab", textRuShift: "Tab", size: "normal", assignment: "functional",
+},
+{
+  key: "q", code: "KeyQ", textEn: "q", textEnShift: "Q", textRu: "й", textRuShift: "Й", size: "normal", assignment: "symbol",
+},
+{
+  key: "w", code: "KeyW", textEn: "w", textEnShift: "W", textRu: "ц", textRuShift: "Ц", size: "normal", assignment: "symbol",
+},
+{
+  key: "e", code: "KeyE", textEn: "e", textEnShift: "E", textRu: "у", textRuShift: "У", size: "normal", assignment: "symbol",
+},
+{
+  key: "r", code: "KeyR", textEn: "r", textEnShift: "R", textRu: "к", textRuShift: "К", size: "normal", assignment: "symbol",
+},
+{
+  key: "t", code: "KeyT", textEn: "t", textEnShift: "T", textRu: "е", textRuShift: "Е", size: "normal", assignment: "symbol",
+},
+{
+  key: "y", code: "KeyY", textEn: "y", textEnShift: "Y", textRu: "н", textRuShift: "Н", size: "normal", assignment: "symbol",
+},
+{
+  key: "u", code: "KeyU", textEn: "u", textEnShift: "U", textRu: "г", textRuShift: "Г", size: "normal", assignment: "symbol",
+},
+{
+  key: "i", code: "KeyI", textEn: "i", textEnShift: "I", textRu: "ш", textRuShift: "Ш", size: "normal", assignment: "symbol",
+},
+{
+  key: "o", code: "KeyO", textEn: "o", textEnShift: "O", textRu: "щ", textRuShift: "Щ", size: "normal", assignment: "symbol",
+},
+{
+  key: "p", code: "KeyP", textEn: "p", textEnShift: "P", textRu: "з", textRuShift: "З", size: "normal", assignment: "symbol",
+},
+{
+  key: "[", code: "BracketLeft", textEn: "[", textEnShift: "{", textRu: "х", textRuShift: "Х", size: "normal", assignment: "symbol",
+},
+{
+  key: "]", code: "BracketRight", textEn: "]", textEnShift: "}", textRu: "ъ", textRuShift: "Ъ", size: "normal", assignment: "symbol",
+},
+{
+  key: "/", code: "Backslash", textEn: "/", textEnShift: "|", textRu: "/", textRuShift: "\\", size: "normal", assignment: "symbol",
+},
+{
+  key: "CapsLock", code: "CapsLock", textEn: "CapsLock", textEnShift: "CapsLock", textRu: "CapsLock", textRuShift: "", size: "big", assignment: "functional",
+},
+{
+  key: "a", code: "KeyA", textEn: "a", textEnShift: "A", textRu: "ф", textRuShift: "Ф", size: "normal", assignment: "symbol",
+},
+{
+  key: "s", code: "KeyS", textEn: "s", textEnShift: "S", textRu: "ы", textRuShift: "Ы", size: "normal", assignment: "symbol",
+},
+{
+  key: "d", code: "KeyD", textEn: "d", textEnShift: "D", textRu: "в", textRuShift: "В", size: "normal", assignment: "symbol",
+},
+{
+  key: "f", code: "KeyF", textEn: "f", textEnShift: "F", textRu: "а", textRuShift: "А", size: "normal", assignment: "symbol",
+},
+{
+  key: "g", code: "KeyG", textEn: "g", textEnShift: "G", textRu: "п", textRuShift: "П", size: "normal", assignment: "symbol",
+},
+{
+  key: "h", code: "KeyH", textEn: "h", textEnShift: "H", textRu: "р", textRuShift: "Р", size: "normal", assignment: "symbol",
+},
+{
+  key: "j", code: "KeyJ", textEn: "j", textEnShift: "J", textRu: "о", textRuShift: "О", size: "normal", assignment: "symbol",
+},
+{
+  key: "k", code: "KeyK", textEn: "k", textEnShift: "K", textRu: "л", textRuShift: "Л", size: "normal", assignment: "symbol",
+},
+{
+  key: "l", code: "KeyL", textEn: "l", textEnShift: "L", textRu: "д", textRuShift: "Д", size: "normal", assignment: "symbol",
+},
+{
+  key: ";", code: "Semicolon", textEn: ";", textEnShift: ":", textRu: "ж", textRuShift: "Ж", size: "normal", assignment: "symbol",
+},
+{
+  key: "'", code: "Quote", textEn: "'", textEnShift: "\"", textRu: "э", textRuShift: "Э", size: "normal", assignment: "symbol",
+},
+{
+  key: "Enter", code: "Enter", textEn: "Enter", textEnShift: "Enter", textRu: "Enter", textRuShift: "", size: "big", assignment: "functional",
+},
+{
+  key: "Shift", code: "ShiftLeft", textEn: "Shift", textEnShift: "Shift", textRu: "Shift", textRuShift: "", size: "big", assignment: "functional",
+},
+{
+  key: "z", code: "KeyZ", textEn: "z", textEnShift: "Z", textRu: "я", textRuShift: "Я", size: "normal", assignment: "symbol",
+},
+{
+  key: "x", code: "KeyX", textEn: "x", textEnShift: "X", textRu: "ч", textRuShift: "Ч", size: "normal", assignment: "symbol",
+},
+{
+  key: "c", code: "KeyC", textEn: "c", textEnShift: "C", textRu: "с", textRuShift: "С", size: "normal", assignment: "symbol",
+},
+{
+  key: "v", code: "KeyV", textEn: "v", textEnShift: "V", textRu: "м", textRuShift: "М", size: "normal", assignment: "symbol",
+},
+{
+  key: "b", code: "KeyB", textEn: "b", textEnShift: "B", textRu: "и", textRuShift: "И", size: "normal", assignment: "symbol",
+},
+{
+  key: "n", code: "KeyN", textEn: "n", textEnShift: "N", textRu: "т", textRuShift: "Т", size: "normal", assignment: "symbol",
+},
+{
+  key: "m", code: "KeyM", textEn: "m", textEnShift: "M", textRu: "ь", textRuShift: "Ь", size: "normal", assignment: "symbol",
+},
+{
+  key: ",", code: "Comma", textEn: ",", textEnShift: "<", textRu: "б", textRuShift: "Б", size: "normal", assignment: "symbol",
+},
+{
+  key: ".", code: "Period", textEn: ".", textEnShift: ">", textRu: "ю", textRuShift: "Ю", size: "normal", assignment: "symbol",
+},
+{
+  key: "/", code: "Slash", textEn: "/", textEnShift: "?", textRu: ".", textRuShift: ",", size: "normal", assignment: "symbol",
+},
+{
+  key: "ArrowUp", code: "ArrowUp", textEn: "⮝", textEnShift: "⮝", textRu: "⮝", textRuShift: "⮝", size: "normal", assignment: "functional",
+},
+{
+  key: "Shift", code: "ShiftRight", textEn: "Shift", textEnShift: "Shift", textRu: "Shift", textRuShift: "Shift", size: "big", assignment: "functional",
+},
+{
+  key: "Control", code: "ControlLeft", textEn: "Ctrl", textEnShift: "Ctrl", textRu: "Ctrl", textRuShift: "Ctrl", size: "normal", assignment: "functional",
+},
+{
+  key: "Meta", code: "MetaLeft", textEn: "⊞", textEnShift: "⊞", textRu: "⊞", textRuShift: "⊞", size: "normal", assignment: "functional",
+},
+{
+  key: "Alt", code: "AltLeft", textEn: "Alt", textEnShift: "Alt", textRu: "Alt", textRuShift: "Alt", size: "normal", assignment: "functional",
+},
+{
+  key: " ", code: "Space", textEn: " ", textEnShift: " ", textRu: " ", textRuShift: " ", size: "tiny", assignment: "symbol",
+},
+{
+  key: "Alt", code: "AltRight", textEn: "Alt", textEnShift: "Alt", textRu: "Alt", textRuShift: "Alt", size: "normal", assignment: "functional",
+},
+{
+  key: "Control", code: "ControlRight", textEn: "Ctrl", textEnShift: "Ctrl", textRu: "Ctrl", textRuShift: "Ctrl", size: "normal", assignment: "functional",
+},
+{
+  key: "ArrowLeft", code: "ArrowLeft", textEn: "⮜", textEnShift: "⮜", textRu: "⮜", textRuShift: "⮜", size: "normal", assignment: "functional",
+},
+{
+  key: "ArrowDown", code: "ArrowDown", textEn: "⮟", textEnShift: "⮟", textRu: "⮟", textRuShift: "⮟", size: "normal", assignment: "functional",
+},
+{
+  key: "ArrowRight", code: "ArrowRight", textEn: "⮞", textEnShift: "⮞", textRu: "⮞", textRuShift: "⮞", size: "normal", assignment: "functional",
+},
+{
+  key: "EN", code: "", textEn: "EN", textEnShift: "EN", textRu: "РУС", textRuShift: "РУС", size: "normal", assignment: "functional",
+}];
+
+class MyKey {
+  constructor(key, code, textEn, textEnShift, textRu, textRuShift, size, assignment) {
+    this.key = key;
+    this.code = code;
+    this.textEn = textEn;
+    this.textEnShift = textEnShift;
+    this.textRu = textRu;
+    this.textRuShift = textRuShift;
+    this.size = size;
+    this.assignment = assignment;
+  }
+}
+
+const VirtualKeyboard = {
+  elements: {
+    container: null,
+    header: null,
+    main: null,
+    mainField: null,
+    mainKeyboard: null,
+    mainNotate: null,
+    button: null,
+    keySet: null,
+    keyBuffer: null,
+  },
+
+  properties: {
+    textValue: "",
+    isCapsLock: false,
+    isShift: false,
+    lang: localStorage.getItem("langPs0m") || "En",
+  },
+
+  init() {
+    this.container = document.createElement("div");
+    this.container.classList.add("container");
+    document.body.prepend(this.container);
+
+    this.header = document.createElement("header");
+    this.header.classList.add("header");
+    this.container.appendChild(this.header);
+    this.header.textContent = "RSS Виртуальная клавиатура";
+
+    this.main = document.createElement("div");
+    this.main.classList.add("main");
+    this.container.appendChild(this.main);
+
+    this.mainField = document.createElement("textarea");
+    this.mainField.classList.add("main__field");
+    this.main.appendChild(this.mainField);
+
+    this.mainKeyboard = document.createElement("div");
+    this.mainKeyboard.classList.add("main__keyboard");
+    this.main.appendChild(this.mainKeyboard);
+
+    this.mainNotate = document.createElement("p");
+    this.mainNotate.classList.add("main__notate");
+    this.main.appendChild(this.mainNotate);
+    this.mainNotate.textContent = `Клавиатура создана в операционной системе Windows.
+    Для переключения языка комбинация: левыe ctrl + alt`;
+
+    this.keySet = [];
+    this.keyBuffer = new Set();
+
+    for (let i = 0; i < 64; i += 1) {
+      const key = new MyKey(
+        ListSetKey[i].key,
+        ListSetKey[i].code,
+        ListSetKey[i].textEn,
+        ListSetKey[i].textEnShift,
+        ListSetKey[i].textRu,
+        ListSetKey[i].textRuShift,
+        ListSetKey[i].size,
+        ListSetKey[i].assignment,
+      );
+      this.keySet.push(key);
+      // console.log(this.keySet[i]);
+
+      this.button = document.createElement("div");
+      this.button.classList.add("key");
+      this.mainKeyboard.append(this.button);
+      this.button.dataset.number = [i];
+      this.button.dataset.code = this.keySet[i].code;
+      if (this.keySet[i].size === "big") {
+        this.button.classList.add("key_big");
+      }
+      if (this.keySet[i].size === "tiny") {
+        this.button.classList.add("key_tiny");
+      }
+      if (this.keySet[i].code === "CapsLock") {
+        this.button.classList.add("key__caps");
+      }
+    }
+    this.setMainFieldFocus();
+  },
+
+  setValue() {
+    // console.log(`isCapsLock  ${this.properties.isCapsLock}`,
+    // `isShift ${this.properties.isShift}`);
+    const language = this.properties.lang;
+    for (let i = 0; i < this.keySet.length; i += 1) {
+      if (!this.properties.isShift && !this.properties.isCapsLock) {
+        this.mainKeyboard.children[i].textContent = this.keySet[i][`text${language}`];
+      }
+      if (this.keySet[i].assignment !== "functional") {
+        if (!this.properties.isShift && this.properties.isCapsLock) {
+          this.mainKeyboard.children[i].textContent = this.keySet[i][`text${language}`].toUpperCase();
+        }
+        if (this.properties.isShift && !this.properties.isCapsLock) {
+          this.mainKeyboard.children[i].textContent = this.keySet[i][`text${language}Shift`].toUpperCase();
+        }
+        if (this.properties.isShift && this.properties.isCapsLock) {
+          this.mainKeyboard.children[i].textContent = this.keySet[i][`text${language}Shift`].toLowerCase();
+        }
+      }
+    }
+  },
+
+  setEventListener() {
+    // document.addEventListener("keydown", (event) => {
+    //   console.log(event);
+
+    //   if (event.getModifierState("CapsLock") && event.code !== "CapsLock") {
+    //     this.properties.isCapsLock = true;
+    //     this.setValue();
+    //     console.log("One time");
+    //   }
+    // }, { once: true });
+
+    document.addEventListener("keydown", (event) => {
+      // console.log(event);
+      // event.preventDefault();
+      // const newClick = new MouseEvent("click", {
+      // bubbles: true,
+      // cancelable: true,
+      // });
+      this.keyBuffer.add(event.code);
+
+      this.checkShortCuts();
+
+      for (let i = 0; i < this.mainKeyboard.children.length; i += 1) {
+        if (event.code === this.mainKeyboard.children[i].dataset.code) {
+          this.mainKeyboard.children[i].classList.add("key_shining");
+        }
+      }
+
+      // if (event.code === "CapsLock" || event.code === "ShiftLeft"
+      //   || event.code === "ShiftRight" || event.code === "ControlLeft"
+      //   || event.code === "ControlRight" || event.code === "AltLeft"
+      //   || event.code === "AltLeft") {
+      //   if (event.repeat) {
+      //     return;
+      //   }
+      // }
+
+      if (event.code === "Tab") {
+        event.preventDefault();
+        this.mainField.value += "    ";
+      }
+
+      if (event.code === "CapsLock") {
+        if (event.repeat) {
+          return;
+        }
+        this.changeActiveCapsLock();
+        this.setValue();
+      }
+
+      if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+        if (event.repeat) {
+          return;
+        }
+        this.properties.isShift = !this.properties.isShift;
+        this.setValue();
+      }
+    });
+
+    document.addEventListener("keyup", (event) => {
+      // console.log(event);
+      this.keyBuffer.delete(event.code);
+      for (let i = 0; i < this.mainKeyboard.children.length; i += 1) {
+        if (event.code === this.mainKeyboard.children[i].dataset.code) {
+          this.mainKeyboard.children[i].classList.remove("key_shining");
+        }
+      }
+      if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+        if (event.repeat) {
+          return;
+        }
+        this.properties.isShift = !this.properties.isShift;
+        this.setValue();
+      }
+    });
+
+    this.mainKeyboard.addEventListener("mousedown", (event) => {
+      if (!event.target.closest(".key")) {
+        return;
+      }
+      this.keyBuffer.add(event.target.dataset.code);
+      console.log(this.keyBuffer);
+      this.checkShortCuts();
+
+      const { number } = event.target.dataset;
+      if (this.keySet[number].assignment !== "functional") {
+        this.mainField.value += this.mainKeyboard.children[number].textContent;
+      }
+      if (this.keySet[number].code === "Enter") {
+        this.mainField.value += "\n";
+      }
+      if (this.keySet[number].code === "Tab") {
+        event.preventDefault();
+        this.mainField.value += "    ";
+      }
+      if (this.keySet[number].code === "CapsLock") {
+        this.changeActiveCapsLock();
+        this.setValue();
+      }
+      if (this.keySet[number].code === "ShiftLeft" || this.keySet[number].code === "ShiftRight") {
+        if (event.repeat) {
+          return;
+        }
+        this.properties.isShift = !this.properties.isShift;
+        this.setValue();
+      }
+    });
+
+    this.mainKeyboard.addEventListener("mouseup", (event) => {
+      if (!event.target.closest(".key")) {
+        return;
+      }
+
+      // this.keyBuffer.delete(event.target.dataset.code);
+      const { number } = event.target.dataset;
+
+      if (this.keySet[number].code === "ShiftLeft" || this.keySet[number].code === "ShiftRight") {
+        if (event.repeat) {
+          return;
+        }
+        this.properties.isShift = !this.properties.isShift;
+        this.setValue();
+      }
+    });
+  },
+
+  checkShortCuts() {
+    console.log(this.keyBuffer);
+    if (this.keyBuffer.has("ControlLeft") && this.keyBuffer.has("AltLeft")) {
+      if (this.properties.lang === "Ru") {
+        this.properties.lang = "En";
+        localStorage.setItem("langPs0m", "En");
+      } else {
+        this.properties.lang = "Ru";
+        localStorage.setItem("langPs0m", "Ru");
+      }
+      this.setValue();
+    }
+    console.log(this.properties.lang);
+  },
+
+  changeActiveCapsLock() {
+    const capsLock = document.querySelector(".key__caps");
+    capsLock.classList.toggle("key__caps_active");
+    this.properties.isCapsLock = !this.properties.isCapsLock;
+  },
+
+  setMainFieldFocus() {
+    this.mainField.focus();
+    this.mainField.addEventListener("blur", () => {
+      this.mainField.focus();
+    });
+  },
+};
+
+VirtualKeyboard.init();
+VirtualKeyboard.setValue();
+VirtualKeyboard.setEventListener();
+// VirtualKeyboard.setMainFieldFocus();
